094947da56cf1032537eca1bfc685fa59d8bfc0b
south-coast-coder <charliejarman1989@yahoo.com>
2022-10-11 15:30:01 +0100

    added js script which changes text below based on which image is clicked, will add in blog texts later

---
+++ js/display.js
@@ -0,0 +1,12 @@
+function display(number)
+{console.log("test")
+if(number==1){
+document.getElementById("about").innerHTML = "<p> html </p>"
+}
+else if (number==2){
+	document.getElementById("about").innerHTML = "<p> git </p>"
+}
+else{
+	console.log("neither")
+}
+}
