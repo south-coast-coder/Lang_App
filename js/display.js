c7a6f5708cf86f5c4ebfabf86ce01854b8449792
south-coast-coder <charliejarman1989@yahoo.com>
2022-12-28 15:05:04 +0000

    changing js to remove scroll

--- js/display.js
+++ js/display.js
@@ -4,17 +4,21 @@ if(number==1){
 	setTimeout(function(){
 document.getElementById("about").innerHTML = " <h1>HTML/CSS </h1>At the moment I am brushing up on my html and css skills so I understand how modern websites are built at least on a basic level"
 },2)
+document.getElementById("blog-cont").scrollIntoView()
 }
 else if (number==2){
 	
 	document.getElementById("about").innerHTML = "<h1>Github </h1>I have created a repository on github and am uploading changes to it from my computer in order to learn the key commands and the general workflow of version control"
+    document.getElementById("blog-cont").scrollIntoView()
 }
 
 else if (number==3){
 	document.getElementById("about").innerHTML = "<h1> Javascript </h1> I am learning Javascript by adding scripts to this page, I have linked to a JS file that allows the content of this box to be updated when the user clicks on one of the images above."
+    document.getElementById("blog-cont").scrollIntoView()
 }
 else if (number ==4){
 	document.getElementById("about").innerHTML = "<h1>Chess</h1> I am improving my Javascript files by building a chess game which I will host on Github Pages... <br> <a href='https://github.com/south-coast-coder/chess-game-2'>Repository Here</a>   <a href='https://south-coast-coder.github.io/chess-game-2/'>Link here</a>"
+    document.getElementById("blog-cont").scrollIntoView()
 }
 else{
 	console.log("neither")
