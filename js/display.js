898bdba3461e6cd7d5f496a75a84958e8318973e
south-coast-coder <charliejarman1989@yahoo.com>
2022-12-28 15:25:57 +0000

    changing js to remove scroll

--- js/display.js
+++ js/display.js
@@ -1,12 +1,12 @@
 function display(number)
 {console.log("test")
 if(number==1){
-
+document.getElementById("me").scrollIntoView({behavior: "auto"})
 document.getElementById("about").innerHTML = " <h1>HTML/CSS </h1>At the moment I am brushing up on my html and css skills so I understand how modern websites are built at least on a basic level"
 document.getElementById("me").scrollIntoView({behavior: "auto"})
 }
 else if (number==2){
-	document.getElementById("me").scrollIntoView()
+	document.getElementById("me").scrollIntoView({behavior: "auto"})
 	document.getElementById("about").innerHTML = "<h1>Github </h1>I have created a repository on github and am uploading changes to it from my computer in order to learn the key commands and the general workflow of version control"
     document.getElementById("me").scrollIntoView({behavior: "auto"})
 }
