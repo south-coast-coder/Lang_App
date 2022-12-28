b50cc22a6832d6b57c5305f9566c16c1d3bef42d
south-coast-coder <charliejarman1989@yahoo.com>
2022-12-28 14:51:26 +0000

    changing js to remove scroll

--- js/display.js
+++ js/display.js
@@ -6,10 +6,10 @@ document.getElementById("about").innerHTML = " <h1>HTML/CSS </h1>At the moment I
 },0)
 }
 else if (number==2){
-	setTimeout(function(){
+	
 	document.getElementById("about").innerHTML = "<h1>Github </h1>I have created a repository on github and am uploading changes to it from my computer in order to learn the key commands and the general workflow of version control"
-},0)
 }
+
 else if (number==3){
 	document.getElementById("about").innerHTML = "<h1> Javascript </h1> I am learning Javascript by adding scripts to this page, I have linked to a JS file that allows the content of this box to be updated when the user clicks on one of the images above."
 }
