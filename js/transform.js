00d489c330e00048c035d76bcb798bd1f1fe79a2
south-coast-coder <charliejarman1989@yahoo.com>
2023-01-01 15:45:32 +0000

    trying to remove default scroll behaviour when user clicks gallery arrows

--- js/transform.js
+++ js/transform.js
@@ -5,7 +5,7 @@ function transform(number){
 		document.getElementById("blog-cont").innerHTML = `<img src=\"forward.png\"style=\"width:2vw;height:2vw;margin-top:7vw;\" class=\"rotateimg180\"></div>
     <div class =\"blog-box\"><img src=\"chess.jpeg\" class=\"placeholder\" onclick=\"display(4);return false\">></div>
     <div class =\"blog-box\"> <img src=\"images/Git-session.png\" class=\"placeholder\" onclick=\"display(2); return false;\"> <div id=\"blog1title\">Github</div></div>
-   <div class =\"blog-box\"><img src=\"js.png\" class=\"placeholder\" onclick=\"display(3)\"></div>
+   <div class =\"blog-box\"><img src=\"js.png\" class=\"placeholder\" onclick=\"display(3); return false\"></div>
     <img src=\"forward.png\" style=\"width:2vw;height:2vw;margin-top:7vw;\" onclick=\"transform(2);return false;\"></div>`
 }
 else if (number ==2){ 
