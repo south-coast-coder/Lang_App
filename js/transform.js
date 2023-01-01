11a5de323647c7fc95d15c9800c29d4dfd31089f
south-coast-coder <charliejarman1989@yahoo.com>
2023-01-01 15:24:23 +0000

    trying to remove default scroll behaviour when user clicks gallery arrows

--- js/transform.js
+++ js/transform.js
@@ -3,7 +3,7 @@ function transform(number){
 		console.log("One")
 		
 		document.getElementById("blog-cont").innerHTML = `<img src=\"forward.png\"style=\"width:2vw;height:2vw;margin-top:7vw;\" class=\"rotateimg180\"></div>
-    <div class =\"blog-box\"><img src=\"chess.jpeg\" class=\"placeholder\" onclick=\"display(4)\">></div>
+    <div class =\"blog-box\"><img src=\"chess.jpeg\" class=\"placeholder\" onclick=\"display(4);return false\">></div>
     <div class =\"blog-box\"> <img src=\"images/Git-session.png\" class=\"placeholder\" onclick=\"display(2); return false;\"> <div id=\"blog1title\">Github</div></div>
    <div class =\"blog-box\"><img src=\"js.png\" class=\"placeholder\" onclick=\"display(3)\"></div>
     <img src=\"forward.png\" style=\"width:2vw;height:2vw;margin-top:7vw;\" onclick=\"transform(2);return false;\"></div>`
@@ -11,7 +11,7 @@ function transform(number){
 else if (number ==2){ 
 	console.log("TWO")
 	document.getElementById("blog-cont").innerHTML =`<img src=\"forward.png\"style=\"width:2vw;height:2vw;margin-top:7vw;\" class=\"rotateimg180\"onclick=\"transform(1);return false\"></div>
-    <div class =\"blog-box\"><img src=\"html.jpeg\" class=\"placeholder\" onclick=\"display(1)\">></div>
+    <div class =\"blog-box\"><img src=\"html.jpeg\" class=\"placeholder\" onclick=\"display(1);return false;\">></div>
     <div class =\"blog-box\"> <img src=\"images/Git-session.png\" class=\"placeholder\" onclick=\"display(2);return false\"> <div id=\"blog1title\">Github</div></div>
    <div class =\"blog-box\"><img src=\"js.png\" class=\"placeholder\" onclick=\"display(3); return false\"></div>
     <img src=\"forward.png\" style=\"width:2vw;height:2vw;margin-top:7vw;>\"</div>`
