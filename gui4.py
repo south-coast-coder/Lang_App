7a65d8eb7f97bde0c209cdea9809384e0d07f017
south-coast-coder <charliejarman1989@yahoo.com>
2023-01-21 20:55:30 +0000

    can now go forwards 1000 wordsand adding page change functionality

--- gui4.py
+++ gui4.py
@@ -1,4 +1,5 @@
 import wx
+import wx.adv
 from translate import *
 from pdf_script import *
 
@@ -7,14 +8,70 @@ import wx.html
 test()
 test2()
 
+wordslist={}
 
 
+
+class Word:
+
+ def __init__(self, original,trans,times=1):
+        self.original=original
+        self.trans=trans
+        self.times=times
+        wordslist[self.original]=self
+ def inc(self):
+        self.times+=1
+
+Word("wizard","guerre",1)
+for key, value in wordslist.items():
+    print(key)
+    print(value.times)
+def checkFreq():
+    for key, value in wordslist.items():
+                            print(key)
+                            print(value.times)
+
+
+usefile="rus_3.pdf"
+use_pdf(usefile)
 current_word="test"
+file=open("new.txt","r")
+data=file.read()   
+print('length file'+str(len(data)))
+words = data.split()
+print(len(words))
+words_len=len(words)
+pages_list=[]
+print("thousand words"+str(words_len/1000))
+thousands=words_len/1000
+for i in range (int(thousands)):  # change this to change no of words per page
+    use_i=i+1
+    ind=use_i*1000
+    pages_list.append(ind)
+print("pages list now"+str(pages_list))
+max_thous=pages_list[-1] 
+print("last thou"+str(max_thous)) # if here then check when forward button clicked and make it do nothing (ie pass)
 
+ten=words[0:1000] # this is extracting words for current page
+twenty=words[1000:2000]
+# print("ten ----- \n"+str(ten))
+page_marker=twenty
+use_page="twenty"
 
 app = wx.PySimpleApp()
 items=["won","two","tree","b","a","b","a","b","a","b","a","b","a","b","a","b","c","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b","b","a","b"]
-frame1=wx.Frame(None, -1, size=(400,400))
+frame1=wx.Frame(None, -1, size=(1000,400))
+
+bitmap = wx.Bitmap('loading.png')
+splash = wx.adv.SplashScreen(
+             bitmap, 
+             wx.adv.SPLASH_CENTER_ON_PARENT|wx.adv.SPLASH_NO_TIMEOUT, 
+             10000,frame1)
+splash.Show()
+
+wx.Yield()
+
+
 
 panel=wx.Panel(frame1, size=(400,400),pos=(0,250))
 
@@ -25,21 +82,37 @@ btn=wx.Button(panel,label="too", size=(100,100), pos=(60,0))
 text1=wx.TextCtrl(panel,1000,value="text", size=(150,20))
 btn2=wx.Button(panel,label="too", size=(100,100), pos=(60,53))
 def OnClickButton(e):
-           
-           print(e)
-           
-           
-           word=trans(current_word)
-           btn.SetLabel(word)
-           text1.SetLabel(word)
 
 
-                
+
+           
+        html.SetPage("<style>a {text-decoration: none;color: red }</style><body></body>") 
+        global use_page
+        global page_marker
+        print("usepage" + use_page)
+        if use_page=="twenty":
+            page_marker=ten 
+            use_page="ten"
+        else:
+            page_marker=twenty
+            use_page="twenty"
+
+        for item in page_marker:
+          html.AppendToPage( "<a href="+item+"> "+item+" </a>")
+
+        
+        
+        
+              
+        
+               
           
 btn2.Bind(wx.EVT_BUTTON,OnClickButton)
 
 
-html = wx.html.HtmlWindow(frame1, size=(400,250))
+html = wx.html.HtmlWindow(frame1, size=(1000,250))
+html.SetStandardFonts(25)
+
 """toolbar=wx.ToolBar(self, -1, size=(10,10),pos=(0,150),style=wx.TB_HORIZONTAL | wx.NO_BORDER)
 toolbar.Realize()"""
 
@@ -50,7 +123,7 @@ toolbar.Realize()"""
 
 #if "gtk2" in wx.PlatformInfo:
 
-html.SetStandardFonts()
+
 dog="dppppg"
 cat="fafafaf"
 kitten="glglg"
@@ -58,11 +131,15 @@ start=0
 
 html.SetPage(
 
-"<style>a {text-decoration: none;color: ; }</style>" #sorry no css support :/]
+"<style>a {text-decoration: none;color: red }</style>" #sorry no css support :/]
 
 
 
 "<a href="+dog+"\">"+dog+"</a> <a href=\"wizard\">wizard</a>.")
+for item in ten:
+       html.AppendToPage(
+            "<a href="+item+"> "+item+" </a>"
+            )
 for item in items:
     if(start==20):
          html.AppendToPage("<br></br> <br></br>")
@@ -76,6 +153,8 @@ for item in items:
 
             )
         start+=1
+
+
 
 
 
@@ -101,8 +180,20 @@ def OnClickWord(e):
                         print("current="+current_word)
                         word=trans(current_word)
                         text1.SetLabel(word)
+                        for key, value in wordslist.items():
+                            if key==current_word:
+                                print("already listed")
+                                value.inc()
+                                print("value now="+str(value.times))
+                                return
+                        Word(current_word,word)
+                        checkFreq()
+
+
+
                         return current_word
 html.Bind(wx.html.EVT_HTML_LINK_CLICKED,OnClickWord)
-
-frame1.Show()
+frame1.Show()  #this and below WERE at bottom
 app.MainLoop()
+
+
